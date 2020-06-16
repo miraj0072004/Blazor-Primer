6cd7cd02b367545bad1a3101a460819c16544d58
miraj0072004@gmail.com <miraj0072004@gmail.com>
2020-06-17 00:11:51 +1000

    Validation of nested complex models and collection types

--- EmployeeManagement.Web/Models/EditEmployeeModel.cs
+++ EmployeeManagement.Web/Models/EditEmployeeModel.cs
@@ -27,7 +27,10 @@ namespace EmployeeManagement.Web.Models
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
         public int DepartmentId { get; set; }
-        public Department Department { get; set; }
+        [ValidateComplexType]
+        public Department Department { get; set; } = new Department();
+
+        
         public string PhotoPath { get; set; }
     }
 }
